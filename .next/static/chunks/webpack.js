3ea6ba7b5aac2ce8603f21ad5da6ec69d2d8d803
Anil <anil.antony@settlin.in>
2022-11-20 23:43:30 +0530

    fix: added changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "51c3bea672ede909"; }
+/******/ 		__webpack_require__.h = function() { return "4faa924c14e8d4ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { blogList } from '../../config/data';
 
 export const getStaticPaths = async () => {
-    const paths = blogList.map(e => {  params: {id: e.id.toString()}})
+    const paths = blogList.map(e => ({ params: {id: e.id.toString()}}))
     return {
         paths,
         fallback: false
@@ -14,21 +14,32 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
     const id = context.params.id;
     let data;
-    blogList.map(e => {e.id === id ? data = e : {}});
+    blogList.map(e => (e.id == id) ? data = e : {});
     return {
         props: {post: data}
     }
 }
 
 export default function PostPage({post}) {
-    console.log(post,'what');
     const router = useRouter();
     const id = router.query.id;
     return(
         <div className={styles.post_container}>
-            <h1>Hi Hello</h1>
-            <Image src={post.cover} alt="cover" width="400" height="400"/>
-
+            <div className={styles.post_banner}>
+                <Image src={post.cover} alt="cover" width="400" height="400"/>
+            </div>
+            <div className={styles.post_wrapper}>
+                <div className={styles.post_middlewrapper}>
+                    <h1>{post.title}</h1>
+                    <span>{post.category}</span>
+                    <p>{post.description}</p>
+                </div>
+                <div className={styles.post_rightwrapper}>
+                    <h2>{post.authorName}</h2>
+                    <Image src = {post.authorAvatar} alt='authorAvatar' width="100" height="100"/>
+                </div>
+            </div>
+            
         </div>
     )
 
