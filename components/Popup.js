e7c9c35c1db66a1112270064c77ae619972ff7a4
Anil Antony <anilantony3134@gmail.com>
2023-05-21 10:33:22 +0530

    feat: added css fixes

--- components/Popup.js
+++ components/Popup.js
@@ -3,7 +3,7 @@ import styles from "../styles/Header.module.css";
 
 const Popup = (props) => {
     return props.trigger ? (
-        <div className={styles.popup}>
+        <div className={styles.popup} onClick={() => props.setTrigger(false)}>
             <div className={styles.innnerpop}>
                 <button className={styles.popupclose} onClick={() => props.setTrigger(false)}>x</button>
                 {props.children}
