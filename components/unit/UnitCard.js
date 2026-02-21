61265a89549fdace5a3d41727708dc25d369fcd4
“sharon1999” <sharonantony51@gmail.com>
2026-02-21 11:40:31 +0530

    refactor: Replace Next.js Image component with a standard img tag in UnitCard and update ash-wednesday post content.

--- components/unit/UnitCard.js
+++ components/unit/UnitCard.js
@@ -1,34 +1,32 @@
-import Image from 'next/image';
-import React from 'react';
-import styles from '../../styles/Unit.module.css';
-import Link from 'next/link';
+import Image from "next/image";
+import React from "react";
+import styles from "../../styles/Unit.module.css";
+import Link from "next/link";
 
 const UnitCard = ({ month, img, file, isUnit = false }) => {
   return (
     <div className={styles.container}>
-      {
-        isUnit ? (
-          <Link href={`gallery/${file}`} key={month}>
-            <Image src={img} alt="" fill style={{ objectFit: 'cover' }} />
-            <div className={styles.overlay}>
-               <p className={styles.title}>{month.substring(0, 15)}</p>
-               <div className={styles.button}> View </div>
+      {isUnit ? (
+        <Link href={`gallery/${file}`} key={month}>
+          <img src={img} alt="" className="object-cover h-full" />
+          <div className={styles.overlay}>
+            <p className={styles.title}>{month.substring(0, 15)}</p>
+            <div className={styles.button}> View </div>
+          </div>
+        </Link>
+      ) : (
+        <>
+          <img src={img} alt="" className="object-cover h-full" />
+          <div className={styles.overlay}>
+            <p className={styles.title}>{month.substring(0, 15)}</p>
+            <div className={styles.button}>
+              <a href={file}> View </a>
             </div>
-          </Link>
-        )
-          :
-          (
-            <>
-              <Image src={img} alt="" fill style={{ objectFit: 'cover' }} />
-              <div className={styles.overlay}>
-                 <p className={styles.title}>{month.substring(0, 15)}</p>
-                 <div className={styles.button}><a href={file}> View </a></div>
-              </div>
-            </>
-          )
-      }
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UnitCard
+export default UnitCard;
